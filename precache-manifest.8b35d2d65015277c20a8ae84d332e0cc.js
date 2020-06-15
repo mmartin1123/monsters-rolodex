02eeb414e3d2d10e70a1e5d06eac70d765bc5c6e
Michael Martin <mictin3@gmail.com>
2020-06-15 12:44:27 -0600

    Updates

---
+++ precache-manifest.8b35d2d65015277c20a8ae84d332e0cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc1e272b9509504c0b2a8cd7ff243377",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "67b6da0acab1c9a5c500",
+    "url": "/monsters-rolodex/static/css/main.96bf2a40.chunk.css"
+  },
+  {
+    "revision": "12240f73676d8f2fbd85",
+    "url": "/monsters-rolodex/static/js/2.a3c82928.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a3c82928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67b6da0acab1c9a5c500",
+    "url": "/monsters-rolodex/static/js/main.95956a82.chunk.js"
+  },
+  {
+    "revision": "6062e4a584c29c6e0372",
+    "url": "/monsters-rolodex/static/js/runtime-main.e3d252a9.js"
+  }
+]);
